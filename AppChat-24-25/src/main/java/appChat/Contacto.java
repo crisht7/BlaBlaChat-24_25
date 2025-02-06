d3c10576770ae83e7f8126b8512ca6639878130b
sofiap55 <sofia.pujante.5@gmail.com>
2025-02-06 19:27:44 +0100

    TODOs de Contacto

--- AppChat-24-25/src/main/java/appChat/Contacto.java
+++ AppChat-24-25/src/main/java/appChat/Contacto.java
@@ -43,18 +43,80 @@ public class Contacto {
     	this.foto = null;
     }
 
-	
-    //TODO: Getters y setters
-    //TODO: eliminar mensajes enviados 
-    //TODO: Añade lista de mensajes al contacto
-    //TODO: Envia un mensaje y lo añade a la lista
-    //TODO: Elimina un mensaje de la lista
-    //TODO: Borra todos los mensajes
     
+  //TODO: Getters y setters
     public String getNombre() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+    
+    public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+    
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public List<Mensaje> getMensajes() {
+		return mensajes;
+	}
+
+	public void setMensajes(List<Mensaje> mensajes) {
+		this.mensajes = mensajes;
+	}
+
+	public ImageIcon getFoto() {
+		return foto;
+	}
+
+	public void setFoto(ImageIcon foto) {
+		this.foto = foto;
+	}
+
+	
+
+	//TODO: Envia un mensaje y lo añade a la lista
+    public void enviarMensaje(Mensaje mensaje) {
+        if (mensaje != null) {
+            mensajes.add(mensaje);
+        }
+    }
+
+  //TODO: eliminar mensajes enviados por un usuario  (??)
+    public void eliminarMensajesEnviadosPor(Usuario usuario) {
+        if (usuario != null) {
+            mensajes.removeIf(mensaje -> mensaje.getEmisor().equals(usuario));
+        }
+    }
+    
+    
+  //TODO: Elimina un mensaje de la lista
+    public boolean eliminarMensaje(Mensaje mensaje) {
+        return mensajes.remove(mensaje);
+    }
+
+  //TODO: Borra todos los mensajes
+    public void borrarTodosLosMensajes() {
+        mensajes.clear();
+    }
+
+    
+  //TODO: Añade lista de mensajes al contacto
+    public void añadirListaMensajes(List<Mensaje> nuevosMensajes) {
+        if (nuevosMensajes != null) {
+            mensajes.addAll(nuevosMensajes);
+        }
+    }
+	
+	
+	
+    
+    
     
 }
     
